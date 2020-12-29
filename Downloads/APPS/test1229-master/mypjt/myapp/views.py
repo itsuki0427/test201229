6440199347b5c716da99957b6402c2266fc470c2
Your Name <you@example.com>
2020-12-29 18:05:10 +0900

    new create

---
+++ Downloads/APPS/test1229-master/mypjt/myapp/views.py
@@ -0,0 +1,24 @@
+from django.views.generic import TemplateView
+class IndexView(TemplateView):
+    template_name = "index.html"
+    
+    def get_context_data(self):
+        ctxt = super().get_context_data()
+        ctxt["username"] = "jiro"
+        return ctxt
+        
+class AboutView(TemplateView):
+    template_name = "about.html"
+
+    def get_context_data(self):
+        ctxt = super().get_context_data()
+        ctxt["num_svc"] = 123456
+        ctxt["skills"] = [
+            "python",
+            "JavaScript",
+            "GoogleAppsScript",
+            "Illustrator",
+            "AfetrEffects",
+            ]
+        return ctxt
+        
